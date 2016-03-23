94f004a61d5b3ec84f5933a5dd65403bd36a8dd5
philmerrell <philmerrell@boisestate.edu>
2016-03-22 22:50:15 -0600

    adds

---
+++ src/admin/settings/hours/hours.controller.js
@@ -0,0 +1,72 @@
+(function() {
+  'use strict';
+  
+  angular
+    .module('app.admin')
+    .controller('HoursCtrl', HoursCtrl)
+  ;
+  
+  function HoursCtrl(AppService) {
+    var vm = this;
+    vm.addHours = addHours;
+    vm.removeHours = removeHours;
+
+    vm.hours = [];
+    vm.newHours = {};
+    
+    ///////////////////////////////////////////////
+
+    activate();
+
+    function activate() {
+      setNavActions();
+      loadHours();
+    }
+
+    function setNavActions() {
+      AppService.setNavTitle('HOURS');
+      AppService.setNavActions({state: 'admin.menuItem', param: {name: 'new'}, title: 'Add hours'});
+    }
+
+
+    function addHours(hours) {
+      vm.hours.push(hours);
+      vm.newHours = {};
+
+    }
+
+    function removeHours(hour) {
+      console.log(hour);
+      _.pull(vm.hours, hour);
+    }
+
+    function loadHours() {
+      vm.hours = [
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Monday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Tuesday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Wednesday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Thursday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Friday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Saturday'
+        },
+        {
+          startTime: '10:00 AM', endTime: '10:00 PM', day: 'Sunday'
+        }
+      ];
+
+      return vm.hours;
+    }
+
+  }
+})();
