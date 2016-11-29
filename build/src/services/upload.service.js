8318cfb32f8f55d1f668701221b29ff17ddffaf0
Phil Merrell <philmerrell@boisestate.edu>
2016-11-29 15:40:00 -0700

    feat: wip Firebase file upload

---
+++ build/src/services/upload.service.js
@@ -0,0 +1,68 @@
+/**
+ * TODO: Limit file type and size.
+ * TODO: Remove old image when a new image is uploaded
+ **/
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.services')
+        .factory('UploadService', UploadService);
+
+    function UploadService() {
+      var storage = null;
+
+      return {
+        setStorageRef       : setStorageRef,
+        uploadImage         : uploadImage,
+        uploadMenuItemImage : uploadMenuItemImage
+      }
+
+      function setStorageRef(uid) {
+        storage = firebase.storage().ref('stores/' + uid + '/');
+      }
+
+      function uploadImage(file) {
+        console.log(file);
+        var deferred = $q.defer();
+        var uploadTask = storage.child('brand/' + file.name).put(file, { contentType: file.type });
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            function(snapshot) {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                // console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED: // or 'paused'
+                        // console.log('Upload is paused');
+                        break;
+                    case firebase.storage.TaskState.RUNNING: // or 'running'
+                        // console.log('Upload is running');
+                        break;
+                }
+            }, function(error) {
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
+
+                    case 'storage/canceled':
+                    // User canceled the upload
+                    break;
+
+                    case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    break;
+                }
+            }, function() {
+                // Upload completed successfully, now we can get the download URL
+                var imageUrl = uploadTask.snapshot.downloadURL;
+                deferred.resolve(imageUrl);
+        });
+        return deferred.promise;
+      }
+    }
+
+});
